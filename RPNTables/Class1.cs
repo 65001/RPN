7d4732680dfdf759c8e65a64da490ac2032c1f30
Abhishek Sathiabalan <asathiabalan@gmail.com>
2018-12-22 14:47:09 -0500

    Created a new Project called Tables
    Tables will allow users to specify an equation with a start and end points alongside a frequencey. It will then generate a table of x and f(x) for the entire range with the specified frequencey.  Additionally it will estimate the Left and Right Riemann sum.

---
+++ RPNTables/Class1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using AbMath.Calculator;
+using CLI;
+
+namespace Solver
+{
+    class Program
+    {
+        private RPN RPN;
+        private static bool DebugMode;
+        private static bool MarkDownMode;
+        private static double PrevAnswer;
+        private static bool SupressOutput = false;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                SupressOutput = true;
+            }
+
+            if (!SupressOutput)
+            {
+                Console.Title = "Math Solver 1.0.4";
+                Console.WindowWidth = Console.BufferWidth;
+                Console.WriteLine("(C) 2018. Abhishek Sathiabalan");
+
+                Console.WriteLine("Recent Changes:");
+                Console.WriteLine("Unary negative is now implemented.");
+                Console.WriteLine("Composite Function bug should now be fixed.");
+                Console.WriteLine("Variadic Functions & Implicit Left bug fixed");
+
+                Console.WriteLine("");
+                Console.WriteLine("Known Bugs:");
+                Console.WriteLine("Space between terms is necessary.");
+                Console.WriteLine("Implicit multiplication.");
+                Console.WriteLine();
+            }
+
+            while (true)
+            {
+                //try
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    string Equation = string.Empty;
+                    string Sstart = string.Empty;
+                    string Send = string.Empty;
+                    string Sfreq = string.Empty;
+
+                    int start = 0;
+                    int end = 0;
+                    double freq = double.NaN;
+                    
+               
+
+                    if (SupressOutput)
+                    {
+                        Equation = args[0];
+                    }
+
+                    while (string.IsNullOrWhiteSpace(Equation))
+                    {
+                        Console.Write("Equation>");
+                        Equation = Console.ReadLine();
+
+                        if (Equation.Length == 0) { Console.Clear(); }
+                    }
+
+                    Console.Write("Start Point>");
+                    Sstart = Console.ReadLine();
+                    start = (int)Calculate(Sstart, 0, 0, 0);
+
+                    Console.Write("End Point>");
+                    Send = Console.ReadLine();
+                    end = (int)Calculate(Send, 0, 0, 0);
+
+                    Console.Write("Freq>");
+                    Sfreq = Console.ReadLine();
+                    freq = Calculate(Sfreq, 0, 0, 0);
+
+
+                    if (MarkDownMode)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Equation>``{Equation}``");
+                    }
+
+                    if (Equation.StartsWith("~"))
+                    {
+                        CLI(Equation);
+                    }
+                    else
+                    {
+                        double Answer = Calculate(Equation, start, end, freq);
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                        PrevAnswer = Answer;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    if (!SupressOutput)
+                    {
+                        Console.Write("Press any key to continue...");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        ///<summary>
+        /// All the Command Line Interpreter Code Here
+        /// </summary>
+        static void CLI(string Equation)
+        {
+
+            StringComparison SC = StringComparison.InvariantCultureIgnoreCase;
+            if (Equation.StartsWith("~functions", SC))
+            {
+                var RPN = new RPN("");
+                foreach (KeyValuePair<string, RPN.Function> KV in RPN.Data.Functions)
+                {
+                    Console.WriteLine(KV.Key);
+                }
+            }
+            else if (Equation.StartsWith("~operators", SC))
+            {
+                var RPN = new RPN("");
+                foreach (KeyValuePair<string, RPN.Operator> KV in RPN.Data.Operators)
+                {
+                    Console.WriteLine(KV.Key);
+                }
+            }
+            else if (Equation.StartsWith("~debug", SC))
+            {
+                DebugMode = !DebugMode;
+                Console.WriteLine($"Debug Mode : {DebugMode}");
+            }
+            else if (Equation.StartsWith("~md"))
+            {
+                MarkDownMode = !MarkDownMode;
+            }
+        }
+
+        ///<summary>
+        /// All the RPN math interactions.
+        /// </summary>
+        static double Calculate(string Equation, int start, int end, double freq)
+        {
+            var RPN = new RPN(Equation);
+            RPN.Data.MarkdownTables = MarkDownMode;
+
+            if (DebugMode)
+            {
+                RPN.Logger += Write;
+            }
+            RPN.Compute();
+
+            PostFix postFix = new PostFix(RPN);
+            if (DebugMode)
+            {
+                postFix.Logger += Write;
+            }
+
+
+            if (RPN.ContainsVariables)
+            {
+                double Rsum = 0;
+                double Lsum = 0;
+                int count = 0;
+                double DeltaX = end - start;
+                double n =  DeltaX / freq;
+                int max = (int)Math.Ceiling(n);
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                Config config = new CLI.Config {Title = "Table"};
+                Tables<string> tables = new Tables<string>(config);
+                tables.Add(new Schema {Column = "x", Width = 10});
+                tables.Add(new Schema {Column = "f(x)", Width = 10});
+
+                Console.WriteLine(tables.GenerateHeaders());
+
+                for (int x = 0; x <= max; x++)
+                {
+                    double RealX = start + count * DeltaX / n;
+                    for (int i = 0; i < RPN.Data.Variables.Count; i++)
+                    {
+                        
+                        if (RPN.Data.Variables[i] == "ans")
+                        {
+                            postFix.SetVariable(RPN.Data.Variables[i], PrevAnswer.ToString());
+                        }
+
+                        if (RPN.Data.Variables[i] == "x")
+                        {
+                            postFix.SetVariable(RPN.Data.Variables[i], (RealX).ToString());
+                        }
+                    }
+                    double answer = postFix.Compute();
+                    if (x < max )
+                    {
+                        Rsum += answer;
+                    }
+
+                    if (count > 0)
+                    {
+                        Lsum += answer;
+                    }
+
+                    PrevAnswer = answer;
+                    tables.Add(new string[] { (RealX).ToString(), answer.ToString(), });
+                    Console.Write( tables.GenerateNextRow() );
+
+                    postFix.Reset();
+                    Console.WriteLine();
+                    count++;
+                }
+
+                Console.WriteLine(tables.GenerateFooter());
+
+                if (tables.SuggestedRedraw)
+                {
+                    Console.WriteLine(tables.Redraw());
+                }
+
+                Console.WriteLine($"Elapsed Time: {sw.ElapsedMilliseconds} (ms)");
+                Console.WriteLine($"Iterations: {count} ");
+                Console.WriteLine();
+
+                Console.WriteLine($"Left Sum : {Rsum}");
+                Console.WriteLine($"Right Sum: {Lsum}");
+                Console.WriteLine($"Left Integral ? : {Rsum * DeltaX/n}");
+                Console.WriteLine($"Right Integral ? : {Lsum * DeltaX/n}");
+                return 0;
+            }
+
+            return  postFix.Compute();
+        }
+
+        static void Write(object sender, string Event)
+        {
+            Console.WriteLine(Event);
+        }
+    }
+}
---
+++ Tables/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using AbMath.Calculator;
+
+namespace Solver
+{
+    class Program
+    {
+        private RPN RPN;
+        private static bool DebugMode;
+        private static bool MarkDownMode;
+        private static double PrevAnswer;
+        private static bool SupressOutput = false;
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                SupressOutput = true;
+            }
+
+            if (!SupressOutput)
+            {
+                Console.Title = "Math Solver 1.0.4";
+                Console.WindowWidth = Console.BufferWidth;
+                Console.WriteLine("(C) 2018. Abhishek Sathiabalan");
+
+                Console.WriteLine("Recent Changes:");
+                Console.WriteLine("Unary negative is now implemented.");
+                Console.WriteLine("Composite Function bug should now be fixed.");
+                Console.WriteLine("Variadic Functions & Implicit Left bug fixed");
+
+                Console.WriteLine("");
+                Console.WriteLine("Known Bugs:");
+                Console.WriteLine("Space between terms is necessary.");
+                Console.WriteLine("Implicit multiplication.");
+                Console.WriteLine();
+            }
+
+            while (true)
+            {
+                //try
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    string Equation = string.Empty;
+
+                    if (SupressOutput)
+                    {
+                        Equation = args[0];
+                    }
+
+                    while (string.IsNullOrWhiteSpace(Equation))
+                    {
+                        Console.Write("Equation>");
+                        Equation = Console.ReadLine();
+
+                        if (Equation.Length == 0) { Console.Clear(); }
+                    }
+
+                    if (MarkDownMode)
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Equation>``{Equation}``");
+                    }
+
+                    if (Equation.StartsWith('~'))
+                    {
+                        CLI(Equation);
+                    }
+                    else
+                    {
+                        double Answer = Calculate(Equation);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (MarkDownMode)
+                        {
+                            Console.WriteLine($"Answer: ``{Answer}``");
+                        }
+                        else if (!SupressOutput)
+                        {
+                            Console.Write("Answer:");
+                            Console.WriteLine(Answer);
+                        }
+                        else
+                        {
+                            Console.WriteLine(Answer);
+                        }
+
+                        PrevAnswer = Answer;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                    }
+
+                    if (!SupressOutput)
+                    {
+                        Console.Write("Press any key to continue...");
+                        Console.ReadKey(true);
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        ///<summary>
+        /// All the Command Line Interpreter Code Here
+        /// </summary>
+        static void CLI(string Equation)
+        {
+
+            StringComparison SC = StringComparison.InvariantCultureIgnoreCase;
+            if (Equation.StartsWith("~functions", SC))
+            {
+                var RPN = new RPN("");
+                foreach (KeyValuePair<string, RPN.Function> KV in RPN.Data.Functions)
+                {
+                    Console.WriteLine(KV.Key);
+                }
+            }
+            else if (Equation.StartsWith("~operators", SC))
+            {
+                var RPN = new RPN("");
+                foreach (KeyValuePair<string, RPN.Operator> KV in RPN.Data.Operators)
+                {
+                    Console.WriteLine(KV.Key);
+                }
+            }
+            else if (Equation.StartsWith("~debug", SC))
+            {
+                DebugMode = !DebugMode;
+                Console.WriteLine($"Debug Mode : {DebugMode}");
+            }
+            else if (Equation.StartsWith("~md"))
+            {
+                MarkDownMode = !MarkDownMode;
+            }
+        }
+
+        ///<summary>
+        /// All the RPN math interactions.
+        /// </summary>
+        static double Calculate(string Equation)
+        {
+            var RPN = new RPN(Equation);
+            RPN.Data.MarkdownTables = MarkDownMode;
+
+            if (DebugMode)
+            {
+                RPN.Logger += Write;
+            }
+            RPN.Compute();
+
+            if (SupressOutput == false && DebugMode == false)
+            {
+                Console.WriteLine("Reverse Polish Notation:");
+                Console.WriteLine(RPN.Polish.Print());
+            }
+
+            PostFix postFix = new PostFix(RPN);
+            if (DebugMode)
+            {
+                postFix.Logger += Write;
+            }
+
+
+            if (RPN.ContainsVariables)
+            {
+                Console.WriteLine("Set the variables");
+                for (int i = 0; i < RPN.Data.Variables.Count; i++)
+                {
+                    if (RPN.Data.Variables[i] == "ans")
+                    {
+                        postFix.SetVariable(RPN.Data.Variables[i], PrevAnswer.ToString());
+                        Console.WriteLine($"{RPN.Data.Variables[i]}={PrevAnswer}");
+                    }
+                    else
+                    {
+                        Console.Write(RPN.Data.Variables[i] + "=");
+                        var VariableExpression = Console.ReadLine();
+                        postFix.SetVariable(RPN.Data.Variables[i], Calculate(VariableExpression).ToString());
+                    }
+                }
+            }
+
+            double answer = postFix.Compute();
+            if (RPN.Data.Format.ContainsKey(answer))
+            {
+                Console.WriteLine($"This answer can also be formatted as {RPN.Data.Format[answer] }");
+            }
+
+            return answer;
+        }
+
+        static void Write(object sender, string Event)
+        {
+            Console.WriteLine(Event);
+        }
+    }
+}
